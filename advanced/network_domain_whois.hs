b55dd2b2cbf4b69cd770d7cfada6a0cd4ce37a48
Marcel-TO <marcel@turobin-ort.at>
2023-07-29 15:39:31 +0200

    network update

--- advanced/network_domain_whois.hs
+++ advanced/network_domain_whois.hs
@@ -1,13 +1,18 @@
-import Network.Simple.TCP (connect, send, recv)
+import Network.Simple.TCP (connect, send, recv, withSocketsDo)
 import qualified Data.ByteString as BS
+import System.IO (hFlush, stdout)
 
+main :: IO ()
 main = do
-	putStr "Domain name: "
-	hFlush stdout --durch puffern kann der string vllt erst nach getline ausgegeben werden
-	domain <- BS.getLine -- returnt bytestring
-	connect "whois.nic.at" "43" $ \(socket, _) -> do
-		send socket domain
-		mbs <- recv socket 4096
-		case mbs of
-			Nothing -> return()
-			Just bs -> BS.putStrLn bs
+  putStr "Domain name: "
+  hFlush stdout -- durch puffern kann der string vllt erst nach getline ausgegeben werden
+  domain <- BS.getLine
+  connect "whois.nic.at" "43" $ \(socket, _) -> do
+    send socket domain
+    recvloop socket
+  where
+    recvloop socket = do
+      mbs <- recv socket 4096
+      case mbs of
+        Just bs -> BS.putStr bs >> recvloop socket
+        Nothing -> return ()
